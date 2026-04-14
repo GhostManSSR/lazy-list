d9c885bf049eaf1379f5265b6e40f2c652ad72f0
Vladimir070142 <artem.ivan456@gmail.com>
2026-04-15 00:20:49 +0700

    Подготовка Business блока

---
+++ components/Business/ListBusiness/index.tsx
@@ -0,0 +1,15 @@
+import Skeleton from "@/components/layout/Skeleton";
+import LoadingSpin from "@/components/layout/LoadingSpin";
+import BlockNews from "@/components/News/BlockNews";
+import Button from "@/components/layout/Button";
+import {FaArrowLeft, FaArrowRight} from "react-icons/fa";
+
+const ListBusiness:React.FC<{}> = () => {
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default ListBusiness
