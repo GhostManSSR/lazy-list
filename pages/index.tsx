98bd2c0d47005fcf1a9660f42bce0ac2ddd45aac
Vladimir070142 <artem.ivan456@gmail.com>
2026-04-15 02:55:56 +0700

    В index страницу добавил основные компоненты

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import ListNews from "@/components/News/ListNews";
+import Business from "@/components/Business/index";
+import ImportantNews from "@/components/ImportantNews";
 
 
 export default function Home() {
   return (
     <>
         <ListNews />
+        <Business/>
+        <ImportantNews/>
     </>
   );
 }
